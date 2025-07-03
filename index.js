bbff93ba8031b98e858c972d4776ab05d6a42a67
vanshika <vanshikasharma1302@gmail.com>
2025-07-03 15:55:53 +0530

    todays code

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs= require ("fs");
+console.log(fs);
+console.log("start");
+setImmediate(()=>{
+    console.log("set Immediate")
+})
+setTimeout(()=>{
+    console.log("settimeout")
+},0)
+fs.readFile("demo.txt","utf-8",(err,data)=>{
+    console.log("file read")
+    console.log(data)
+    setTimeout(()=>{
+        console.log("timer 2");
+    },0)
+    setImmediate(()=>{
+        console.log("immediate 2");
+    })
+})
+function someTask(){
+    return new Promise((resolve,reject)=>{
+        resolve ("promise")
+    })
+}
+someTask().then((data)=>{
+    console.log(data)
+})
+.catch((err)=>{
+    console.log(err)
+})
+process.nextTick(()=>{
+    console.log("next tick")
+})
+console.log("end");
